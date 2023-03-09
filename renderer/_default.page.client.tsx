55e53c319082c34773997e34db380ebd3fb19cfa
fredlun <fred.lunden@authority.se>
2023-03-09 16:38:50 +0100

    initial commit

---
+++ renderer/_default.page.client.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { hydrateRoot } from 'react-dom/client'
+import { PageLayout } from './PageLayout'
+import type { PageContextClient } from './types'
+
+export { render }
+
+async function render(pageContext: PageContextClient) {
+  const { Page, pageProps } = pageContext
+  hydrateRoot(
+    document.getElementById('page-view')!,
+    <PageLayout pageContext={pageContext}>
+      <Page {...pageProps} />
+    </PageLayout>
+  )
+}
+
+/* To enable Client-side Routing:
+export const clientRouting = true
+// !! WARNING !! Before doing so, read https://vite-plugin-ssr.com/clientRouting */
